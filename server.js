59a5a06d97f817d032f5687135dc41eaab043569
Curtis Halls <curtishalls@ymail.com>
2025-11-14 20:55:48 -0700

    I hate typos

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
     );
-    res.setHeader('access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });
 
