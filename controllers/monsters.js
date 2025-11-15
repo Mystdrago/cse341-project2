1d17aa47ad4fd4bfa3776f93809cd28dd9e1210d
Curtis Halls <curtishalls@ymail.com>
2025-11-14 21:56:34 -0700

    ugh

--- controllers/monsters.js
+++ controllers/monsters.js
@@ -1,76 +1,93 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
+const validateMonster = (data) => {
+    if (!data.name || typeof data.name !== 'string') return 'Name is required and must be a String';
+    if (!data.level || typeof data.level !== 'string') return 'Level is required and must be a String';
+    if (!data.size || typeof data.size !== 'string') return 'Size is required and must be a String';
+    if (!data.types || typeof data.types !== 'string') return 'Types is required and must be a String';
+    if (!data.healthPoints || typeof data.healthPoints !== 'string') return 'Health Points is required and must be a String';
+    if (!data.staminaPoints || typeof data.staminaPoints !== 'string') return 'Stamina Points is required and must be a String';
+    if (!data.attentionPoints || typeof data.attentionPoints !== 'string') return 'Attention Points is required and must be a String';
+    if (!data.luckyPoints || typeof data.luckyPoints !== 'string') return 'Lucky Points is required and must be a String';
+    return null;
+}
+
 const getAll = async (req, res) => {
-    //#swagger.tags=['monsters']
-    const result = await mongodb.getDatabase().db().collection('monsters').find();
-    result.toArray().then((monsters) => {
-        res.setHeader('Content-Type', 'application/json');
+    //swagger.tags=['monsters']
+    try {
+        const monsters = await mongodb.getDatabase().db().collection('monsters').find().toArray();
         res.status(200).json(monsters);
-    });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
 
 const getSingle = async (req, res) => {
-    //#swagger.tags=['monsters']
-    const monsterId = new ObjectId(String(req.params.id));
-    const result = await mongodb.getDatabase().db().collection('monsters').find({_id: monsterId});
-    result.toArray().then((monsters) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(monsters[0]);
-    });
+    //swagger.tags=['monsters']
+    try {
+        const monsterId = new ObjectId(req.params.id);
+        const monster = await mongodb.getDatabase().db().collection('monsters').findOne({_id: monsterId});
+        if (!monster) return res.status(404).json({ error: 'Monster not found' });
+        res.status(200).json(monster);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
 
 const addMonster = async (req, res) => {
-    //#swagger.tags=['monsters']
-    const monster = {
-        name: req.body.name,
-        level: req.body.level,
-        size: req.body.size,
-        types: req.body.types,
-        healthPoints: req.body.healthPoints,
-        staminaPoints: req.body.staminaPoints,
-        attentionPoints: req.body.attentionPoints,
-        luckyPoints: req.body.luckyPoints
-    };
-    const response = await mongodb.getDatabase().db().collections('monsters').insertOne(monster);
-    if (response.acknowledged) {
-    res.status(204).send();
-  }
-  else {
-    res.status(500).json(response.error || 'Some error occured while adding the monster.')
-  }
+    //swagger.tags=['monsters']
+    try {
+        const error = validateMonster(req.body);
+        if (error) return res.status(400).json({ error });
+
+        const monster = { ...req.body };
+        const response = await mongodb.getDatabase().db().collection('monsters').insertOne(monster);
+        if (response.acknowledged) {
+            res.status(201).json({ message: 'Monster created', id: response.insertedId });
+        } else {
+            res.status(500).json({ error: 'Failed to add monster' });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
 
 const updateMonster = async (req, res) => {
-    //#swagger.tags=['monsters']
-  const monsterId = new ObjectId(String(req.params.id));
-  const monster = {
-    name: req.body.name,
-        level: req.body.level,
-        size: req.body.size,
-        types: req.body.types,
-        healthPoints: req.body.healthPoints,
-        staminaPoints: req.body.staminaPoints,
-        attentionPoints: req.body.attentionPoints,
-        luckyPoints: req.body.luckyPoints
-  };
-  const response = await mongodb.getDatabase().db().collection('monsters').replaceOne({_id: monsterId}, monster);
-    if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occured while updating the monster.')
-  }
+    //swagger.tags=['monsters']
+    try {
+        const error = validateMonster(req.body);
+        if (error) return res.status(400).json({ error });
+
+        const monsterId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('monsters').replaceOne(
+            { _id: monsterId },
+            { ...req.body }
+        );
+
+        if (response.modifiedCount > 0) {
+            res.status(200).json({ message: 'Monster updated' });
+        } else {
+            res.status(404).json({ error: 'Monster not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
 
 const removeMonster = async (req, res) => {
-    //#swagger.tags=['monsters']
-  const monsterId = new ObjectId(String(req.params.id));
-  const response = await mongodb.getDatabase().db().collection('monsters').deleteOne({_id: monsterId});
-    if (response.deletedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occured while removing the monster.')
-  }
+    //swagger.tags=['monsters']
+    try {
+        const monsterId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('monsters').deleteOne({ _id: monsterId });
+        if (response.deletedCount > 0) {
+            res.status(200).json({ message: 'Monster deleted' });
+        } else {
+            res.status(404).json({ error: 'Monster not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
 
 module.exports = {
--- controllers/players.js
+++ controllers/players.js
@@ -1,77 +1,94 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
+const validateplayer = (data) => {
+    if (!data.name || typeof data.name !== 'string') return 'Name is required and must be a String';
+    if (!data.level || typeof data.level !== 'string') return 'Level is required and must be a String';
+    if (!data.size || typeof data.size !== 'string') return 'Size is required and must be a String';
+    if (!data.species || typeof data.species !== 'string') return 'Species is required and must be a String';
+    if (!data.types || typeof data.types !== 'string') return 'Types is required and must be a String';
+    if (!data.healthPoints || typeof data.healthPoints !== 'string') return 'Health Points is required and must be a String';
+    if (!data.staminaPoints || typeof data.staminaPoints !== 'string') return 'Stamina Points is required and must be a String';
+    if (!data.attentionPoints || typeof data.attentionPoints !== 'string') return 'Attention Points is required and must be a String';
+    if (!data.luckyPoints || typeof data.luckyPoints !== 'string') return 'Lucky Points is required and must be a String';
+    return null;
+}
+
 const getAll = async (req, res) => {
-    //#swagger.tags=['players']
-    const result = await mongodb.getDatabase().db().collection('players').find();
-    result.toArray().then((players) => {
-        res.setHeader('Content-Type', 'application/json');
+    //swagger.tags=['players']
+    try {
+        const players = await mongodb.getDatabase().db().collection('players').find().toArray();
         res.status(200).json(players);
-    });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
 
 const getSingle = async (req, res) => {
-    //#swagger.tags=['players']
-    const playerId = new ObjectId(String(req.params.id));
-    const result = await mongodb.getDatabase().db().collection('players').find({_id: playerId});
-    result.toArray().then((players) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(players[0]);
-    });
+    //swagger.tags=['players']
+    try {
+        const playerId = new ObjectId(req.params.id);
+        const player = await mongodb.getDatabase().db().collection('players').findOne({_id: playerId});
+        if (!player) return res.status(404).json({ error: 'player not found' });
+        res.status(200).json(player);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
 
 const addPlayer = async (req, res) => {
-    //#swagger.tags=['players']
-    const player = {
-        name: req.body.name,
-        level: req.body.level,
-        size: req.body.size,
-        species: req.body.species,
-        types: req.body.types,
-        healthPoints: req.body.healthPoints,
-        staminaPoints: req.body.staminaPoints,
-        attentionPoints: req.body.attentionPoints,
-        luckyPoints: req.body.luckyPoints
-    };
-    const response = await mongodb.getDatabase().db().collections('players').insertOne(player);
-    if (response.acknowledged) {
-    res.status(204).send();
-  }
-  else {
-    res.status(500).json(response.error || 'Some error occured while adding the player.')
-  }
+    //swagger.tags=['players']
+    try {
+        const error = validateplayer(req.body);
+        if (error) return res.status(400).json({ error });
+
+        const player = { ...req.body };
+        const response = await mongodb.getDatabase().db().collection('players').insertOne(player);
+        if (response.acknowledged) {
+            res.status(201).json({ message: 'player created', id: response.insertedId });
+        } else {
+            res.status(500).json({ error: 'Failed to add player' });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
 
 const updatePlayer = async (req, res) => {
-    //#swagger.tags=['players']
-  const playerId = new ObjectId(String(req.params.id));
-  const player = {
-    name: req.body.name,
-        level: req.body.level,
-        size: req.body.size,
-        types: req.body.types,
-        healthPoints: req.body.healthPoints,
-        staminaPoints: req.body.staminaPoints,
-        attentionPoints: req.body.attentionPoints,
-        luckyPoints: req.body.luckyPoints
-  };
-  const response = await mongodb.getDatabase().db().collection('players').replaceOne({_id: playerId}, player);
-    if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occured while updating the player.')
-  }
+    //swagger.tags=['players']
+    try {
+        const error = validateplayer(req.body);
+        if (error) return res.status(400).json({ error });
+
+        const playerId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('players').replaceOne(
+            { _id: playerId },
+            { ...req.body }
+        );
+
+        if (response.modifiedCount > 0) {
+            res.status(200).json({ message: 'player updated' });
+        } else {
+            res.status(404).json({ error: 'player not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
 
 const removePlayer = async (req, res) => {
-    //#swagger.tags=['players']
-  const playerId = new ObjectId(String(req.params.id));
-  const response = await mongodb.getDatabase().db().collection('players').deleteOne({_id: playerId});
-    if (response.deletedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Some error occured while removing the player.')
-  }
+    //swagger.tags=['players']
+    try {
+        const playerId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('players').deleteOne({ _id: playerId });
+        if (response.deletedCount > 0) {
+            res.status(200).json({ message: 'player deleted' });
+        } else {
+            res.status(404).json({ error: 'player not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
 
 module.exports = {
